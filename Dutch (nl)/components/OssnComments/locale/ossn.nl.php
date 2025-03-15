3068f87069c33547c40ddcff7c82a3b2e465ca27
lianglee <manactric@gmail.com>
2025-03-15 23:49:55 +0500

    Fixes

--- Dutch (nl)/components/OssnComments/locale/ossn.nl.php
+++ Dutch (nl)/components/OssnComments/locale/ossn.nl.php
@@ -1,18 +1,23 @@
 <?php
 /**
-* Translated locale
-* ossn.nl.php
-**/
-
-ossn_register_languages('nl', array(
-	 'write:comment' => 'Commentaar schrijven ...', 
-	 'like' => 'Zoals', 
-	 'unlike' => 'Anders dan', 
-	 'comment:deleted' => 'Reactie is gewist!', 
-	 'comment:delete:error' => 'Commentaar kan niet worden verwijderd. Probeer het later opnieuw.', 
-	 'comment:delete' => 'Wissen', 
-	 'comment:comment' => 'Opmerking', 
-	 'comment:view:all' => 'Alle reacties bekijken', 
-	 'comment:edit:success' => 'Reactie is gewijzigd', 
-	 'comment:edit:failed' => 'Uw reactie kan niet worden bewerkt', 
-));
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network (OSSN)
+ * @author    Dieter <info@marohn.nl>
+ * @copyright 2014-2018 OpenTeknik LLC
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$nl = array(
+	'write:comment' => 'Reageer...',
+	'like' => 'Leuk',
+	'unlike' => 'Niet meer leuk',
+	'comment:deleted' => 'Reactie verwijderd!',
+	'comment:delete:error' => 'Reactie kon niet worden verwijderd! probeer het later opnieuw.',
+	'comment:delete' => 'verwijderen',
+	'comment:comment' => 'Reactie',
+	'comment:view:all' => 'Alle reacties weergeven',
+	'comment:edit:success' => 'Je reactie is aangepast',
+	'comment:edit:failed' => 'Kan je reactie niet aanpassen',
+);
+ossn_register_languages('nl', $nl); 
