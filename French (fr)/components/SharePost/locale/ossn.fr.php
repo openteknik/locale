d752655eb76537f2f8abba5a480ffab92715ae7b
lianglee <manactric@gmail.com>
2026-02-15 21:03:26 +0500

    Fixes

--- French (fr)/components/SharePost/locale/ossn.fr.php
+++ French (fr)/components/SharePost/locale/ossn.fr.php
@@ -1,24 +1,32 @@
 <?php
 /**
-* Translated locale
-* ossn.fr.php
-**/
-
-ossn_register_languages('fr', array(
-	 'post:share' => 'Partager', 
-	 'post:shared' => 'La mission a été partagée', 
-	 'post:share:error' => 'La poste ne peut pas être partagée', 
-	 'post:shared:title' => 'Poste partagé', 
-	 'post:share:unavailable' => 'Cette pièce jointe peut avoir été retirée ou la personne qui a partagé le document peut ne pas avoir la permission de la partager avec vous.', 
-	 'post:share:type' => 'Sélectionner un type', 
-	 'post:share:selectfriend' => 'Sélectionner un ami', 
-	 'post:share:selectgroup' => 'Sélectionner un groupe', 
-	 'post:share:newsfeed' => 'Flux de nouvelles', 
-	 'post:share:friend' => 'Profil d\'ami', 
-	 'post:share:group' => 'Groupe', 
-	 'post:shared:event:title' => 'Partagé un événement', 
-	 'post:shared:poll:title' => 'Partage d\'un sondage', 
-	 'post:shared:video:title' => 'Vidéo partagée %s', 
-	 'post:shared:file:title' => 'Partagé un fichier', 
-	 'post:shared:marketplace:product' => 'Partagé un produit', 
-));
+ * Open Source Social Network
+ *
+ * @package Open Source Social Network
+ * @author    OSSN Core Team <info@openteknik.com>
+ * @copyright 2014-2016 OPENTEKNIK LLC
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$fr = array(
+		'post:share' => 'Partager',
+		'post:shared' => 'La publication a été partagée',
+		'post:share:error' => 'La publication ne peut pas être partagée',
+		'post:shared:title' => 'Publication partagée',
+		'post:share:unavailable' => 'Cette pièce jointe a peut-être été supprimée ou la personne qui l’a partagée n’a pas l’autorisation de la partager avec vous.',
+		'post:share:type' => 'Sélectionner le type',
+		'post:share:selectfriend' => 'Sélectionner un ami',
+		'post:share:selectgroup' => 'Sélectionner un groupe',
+		
+		'post:share:newsfeed' => 'Fil d’actualité',
+		'post:share:friend' => 'Profil de l’ami',
+		'post:share:group' => 'Groupe',
+		'post:shared:event:title' => 'A partagé un événement',
+		'post:shared:poll:title' => 'A partagé un sondage',
+		'post:shared:video:title' => 'A partagé une vidéo %s',
+		'post:shared:file:title' => 'A partagé un fichier',
+		'post:shared:marketplace:product' => 'A partagé un produit',
+		'post:shared:from:group' => 'A partagé une publication d’un groupe',
+		'post:shared:from:businesspage' => 'A partagé une publication d’une page',
+);
+ossn_register_languages('fr', $fr);
--- German (de)/components/SharePost/locale/ossn.de.php
+++ German (de)/components/SharePost/locale/ossn.de.php
@@ -1,24 +1,32 @@
 <?php
 /**
-* Translated locale
-* ossn.de.php
-**/
-
-ossn_register_languages('de', array(
-	 'post:share' => 'Gemeinsam nutzen', 
-	 'post:shared' => 'Der Beitrag wurde gemeinsam genutzt', 
-	 'post:share:error' => 'Post kann nicht gemeinsam genutzt werden', 
-	 'post:shared:title' => 'Gemeinsam genutzte Post', 
-	 'post:share:unavailable' => 'Dieser Anhang wurde möglicherweise entfernt, oder die Person, die sie gemeinsam benutzt hat, ist möglicherweise nicht berechtigt, sie mit Ihnen gemeinsam zu nutzen.', 
-	 'post:share:type' => 'Typ auswählen', 
-	 'post:share:selectfriend' => 'Freund auswählen', 
-	 'post:share:selectgroup' => 'Gruppe auswählen', 
-	 'post:share:newsfeed' => 'Newsfeed', 
-	 'post:share:friend' => 'Freund-Profil', 
-	 'post:share:group' => 'Gruppe', 
-	 'post:shared:event:title' => 'Gemeinsam genutztes Ereignis', 
-	 'post:shared:poll:title' => 'Umfrage gemeinsam nutzen', 
-	 'post:shared:video:title' => 'Gemeinsam genutztes Video %s', 
-	 'post:shared:file:title' => 'Gemeinsam genutzte Datei', 
-	 'post:shared:marketplace:product' => 'Gemeinsam genutztes Produkt', 
-));
+ * Open Source Social Network
+ *
+ * @package Open Source Social Network
+ * @author    OSSN Core Team <info@openteknik.com>
+ * @copyright 2014-2016 OPENTEKNIK LLC
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$de = array(
+		'post:share' => 'Teilen',
+		'post:shared' => 'Der Beitrag wurde geteilt',
+		'post:share:error' => 'Der Beitrag kann nicht geteilt werden',
+		'post:shared:title' => 'Geteilter Beitrag',
+		'post:share:unavailable' => 'Dieser Anhang wurde möglicherweise entfernt oder die Person, die ihn geteilt hat, hat keine Berechtigung, ihn mit dir zu teilen.',
+		'post:share:type' => 'Typ auswählen',
+		'post:share:selectfriend' => 'Freund auswählen',
+		'post:share:selectgroup' => 'Gruppe auswählen',
+		
+		'post:share:newsfeed' => 'Neuigkeiten',
+		'post:share:friend' => 'Freundesprofil',
+		'post:share:group' => 'Gruppe',
+		'post:shared:event:title' => 'Hat ein Ereignis geteilt',
+		'post:shared:poll:title' => 'Hat eine Umfrage geteilt',
+		'post:shared:video:title' => 'Hat ein Video geteilt %s',
+		'post:shared:file:title' => 'Hat eine Datei geteilt',
+		'post:shared:marketplace:product' => 'Hat ein Produkt geteilt',
+		'post:shared:from:group' => 'Hat einen Beitrag aus einer Gruppe geteilt',
+		'post:shared:from:businesspage' => 'Hat einen Beitrag von einer Seite geteilt',
+);
+ossn_register_languages('de', $de);
