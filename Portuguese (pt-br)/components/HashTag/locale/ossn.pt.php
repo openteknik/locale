76d1ee6df8151395c9434274309d295fb8a9accd
Arsalan Shah <manactric@gmail.com>
2026-02-26 20:54:08 +0500

    Create ossn.pt.php

---
+++ Portuguese (pt-br)/components/HashTag/locale/ossn.pt.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    OSSN Core Team <info@openteknik.com>
+ * @copyright (C) OpenTeknik LLC
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$pt = array(
+	'hashtag:trending' => 'Em tendência',
+	'hashtag:counter' => '%s publicações no mural',
+);
+ossn_register_languages('pt', $pt);
