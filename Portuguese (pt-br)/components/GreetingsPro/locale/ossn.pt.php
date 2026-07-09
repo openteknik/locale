5c758ee05d46018d77308dd5268b38f3c02d306b
lianglee <manactric@gmail.com>
2026-07-09 09:48:13 +0500

    chnages

---
+++ Portuguese (pt-br)/components/GreetingsPro/locale/ossn.pt.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package    Open Source Social Network
+ * @author     Open Social Website Core Team <info@openteknik.com>
+ * @copyright  (C) OpenTeknik LLC
+ * @license    Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link       https://www.opensource-socialnetwork.org/
+ */
+
+$pt = array(
+    'greetings:widget:morning'       => 'Bom dia',
+    'greetings:widget:morning:sub'   => 'Comece o dia com energia renovada! Que a sua manhã seja focada, produtiva e feliz.',
+    
+    'greetings:widget:afternoon'     => 'Boa tarde',
+    'greetings:widget:afternoon:sub' => 'Que a sua tarde seja leve, abençoada, iluminada, produtiva e feliz.',
+    
+    'greetings:widget:evening'       => 'Boa noite',
+    'greetings:widget:evening:sub'   => 'À medida que o dia termina, confira as principais discussões no seu feed.',
+    
+    'greetings:widget:night'         => 'Boa noite',
+    'greetings:widget:night:sub'     => 'Desejamos a você uma noite tranquila. Hora de relaxar e ver o que os seus amigos estão aprontando.',
+);
+ossn_register_languages('pt', $pt);
---
+++ Portuguese (pt-br)/components/WeatherPro/locale/ossn.pt.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package    Open Source Social Network
+ * @author     Open Social Website Core Team <info@openteknik.com>
+ * @copyright  (C) OpenTeknik LLC
+ * @license    Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link       https://www.opensource-socialnetwork.org/
+ */
+
+ossn_register_languages('pt', array(
+		'weatherpro:enter:valid:city'         => 'Por favor, selecione primeiro um mapeamento de cidade válido nos itens do menu suspenso.',
+		'weatherpro:settings:saved'           => 'As configurações foram salvas!',
+		'weather:pro:title'                   => 'Clima',
+		'weather:pro:settings'                => 'Configurações',
+		'weather:pro:search:city'             => 'Buscar Cidade:',
+		'weather:pro:placeholder:city'        => 'Digite o nome da cidade...',
+		'weather:pro:latitude'                => 'Latitude:',
+		'weather:pro:longitude'               => 'Longitude:',
+		'weather:pro:placeholder:autofill'    => 'Preenchido automaticamente',
+		'weather:pro:temp:unit'               => 'Unidade de Temperatura:',
+		'weather:pro:unit:celsius'            => 'Celsius (&deg;C)',
+		'weather:pro:unit:fahrenheit'         => 'Fahrenheit (&deg;F)',
+		'weather:pro:save'                    => 'Salvar Configurações',
+		'weather:pro:loading'                 => 'Carregando...',
+		'weather:pro:icon:alt'                => 'Ícone do Clima',
+		'weather:pro:feels:like'              => 'Sensação térmica:',
+		'weatherpro:admin:title'              => 'Configuração do Weather Widget Pro',
+		'weatherpro:admin:how_to_obtain'      => 'Como obter sua Chave de API:',
+		'weatherpro:admin:step1'              => 'Cadastre-se ou faça login em %s.',
+		'weatherpro:admin:step2'              => 'Navegue até a aba "My API Keys" (Minhas Chaves de API) na visualização de configurações do painel do seu perfil.',
+		'weatherpro:admin:step3'              => 'Dê um nome ao seu identificador de token, clique em Gerar, copie o token da chave e cole-o no campo de layout abaixo.',
+		'weatherpro:admin:note'               => 'Nota: Chaves de desenvolvedor totalmente novas podem levar até 1 hora para se tornarem globalmente ativas nos clusters de servidores deles.',
+		'weatherpro:api:error:coordinates'    => 'Pares de configuração de coordenadas ausentes',
+		'weatherpro:api:error:not_configured' => 'O componente não está configurado!',
+		'weatherpro:api:error:fetch_failed'   => 'Falha ao buscar métricas do nó remoto',
+));
